f8557f98b5d225e8564d8a4489b758fdbae1fe22
Prigwarn <gmprigwarn@gmail.com>
2021-01-24 22:22:14 +0700

    fixbug

--- app/src/main/java/com/stou/mylogin6096007544/LoginActivity.kt
+++ app/src/main/java/com/stou/mylogin6096007544/LoginActivity.kt
@@ -1,12 +1,14 @@
 package com.stou.mylogin6096007544
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LoginActivity : AppCompatActivity() {
     var builder: AlertDialog.Builder? = null
@@ -14,6 +16,7 @@ class LoginActivity : AppCompatActivity() {
     var pass:EditText? = null
     var login_fail : TextView?=null
     var countLogin = 5
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -23,7 +26,7 @@ class LoginActivity : AppCompatActivity() {
         val login: Button = findViewById(R.id.login)
         login.setOnClickListener {
             if (validateData(txt_login!!, pass!!)){
-                if ( Login(txt_login!!,pass!!)){
+                if ( Login(txt_login!!, pass!!, countLogin)){
                     val intent = Intent(this, WelcomeAcitivity::class.java)
                     intent.putExtra("username", txt_login!!.text.toString())
                     startActivity(intent)
@@ -31,27 +34,45 @@ class LoginActivity : AppCompatActivity() {
                     countLogin--
                     if (countLogin==0){
                         val intent = Intent(this, ErrorActivity::class.java)
-
                         startActivity(intent)
                     }else {
+
                         login_fail!!.text = "No. of attempts remaining :$countLogin"
+                        login_fail!!.visibility = View.VISIBLE
                     }
                 }
             }
         }
     }
-    private  fun Login(name: EditText, pass: EditText) :Boolean{
+
+    private  fun Login(name: EditText, pass: EditText, count: Int) :Boolean{
         if (name.text.toString() != "admin"){
-            showDialog("ชื่อผู้ใช้ไม่ถูกต้อง..!!!!")
+            if (count>1){
+                showDialog("ชื่อผู้ใช้ไม่ถูกต้อง..!!!!")
+            }
+            name.setText("")
+            pass.setText("")
             name.requestFocus()
             return false
         }
+
         if (pass.text.toString()!="1234"){
-            showDialog("password ไม่ถูกต้อง")
+            if (count>1){
+                showDialog("password ไม่ถูกต้อง")
+            }
+            pass.setText("")
             pass.requestFocus()
             return false
         }
         return true
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (countLogin<=0){
+        countLogin = 5
+        login_fail!!.visibility = View.GONE}
 
     }
 
@@ -71,9 +92,9 @@ class LoginActivity : AppCompatActivity() {
     private  fun showDialog(data: String){
         builder = AlertDialog.Builder(this)
         builder!!.setMessage(data)
-            .setCancelable(false)
-            .setPositiveButton("ตกลง") { _, _ ->
-            }
+                .setCancelable(false)
+                .setPositiveButton("ตกลง") { _, _ ->
+                }
         val alert = builder!!.create()
         alert.setTitle("Error...!!!!!")
         alert.show()
